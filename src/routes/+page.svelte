7ac275bee7c83b55afc5329e3a0a29e4afc8c0e3
SaschaDoe <sascha.doerflein@web.de>
2022-09-01 13:57:24 +0200

    Setup Svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,60 @@
+<script>
+	const handleNewWorldClick = () => {
+		window.alert("New World Not implemented yet")
+	};
+
+	const handleLoadWorldClick = () => {
+		window.alert("Load World Not implemented yet")
+	};
+</script>
+
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Random tables for rpg" />
+</svelte:head>
+
+<section>
+	<div class="btn-group">
+		<div class="inner-btn-group">
+			<h1>RPG Creator</h1>
+			<button on:click={handleNewWorldClick}>New World</button>
+			<button on:click={handleLoadWorldClick}>Load World</button>
+		</div>
+	</div>
+</section>
+
+<style>
+	section {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+		
+	}
+
+	.btn-group{
+		text-align: center;  
+		position: fixed;
+		top: 33%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+
+	.inner-btn-group{
+		position:absolute;
+		width: 100px;
+		top:50%;
+		height:10em;
+		margin-top:-5em
+	}
+
+</style>
